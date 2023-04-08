2aa8bf0724515999fcfdb8445e994b4f33dce56b
Adrian <Adrian.Carrasco2@alu.uclm.es>
2023-04-08 04:19:51 +0200

    mas avances

--- include/word.h
+++ include/word.h
@@ -4,9 +4,6 @@
 #include <string>
 
 class Word {
-public:
-    //void saludar();
-
 private:
         class Fragment {
         public:
@@ -17,9 +14,42 @@ private:
 
         Fragment fragment;
         int line;
+        int pos;
         std::string previous_word;
         std::string later_word;
-    
+
+public:
+    //void saludar();
+
+    //Para ordenar al revés, cambiar < por >
+    bool operator<(const Word& other) const{
+        if (fragment.thread_number != other.fragment.thread_number)
+            return fragment.thread_number < other.fragment.thread_number;
+
+        else if (line != other.line)
+            return line < other.line;
+        else
+            return pos < other.pos;
+                 
+    }
+
+    Word(int thread_number, int line_i, int line_f, int line, std::string previous_word, std::string later_word, int pos)
+    {
+        this->fragment.thread_number = thread_number;
+        this->fragment.line_i = line_i;
+        this->fragment.line_f = line_f;
+        this->line = line;
+        this->previous_word = previous_word;
+        this->later_word = later_word;
+        this->pos = pos;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Word& p) {
+        os << "[Hilo: " << p.fragment.thread_number << " inicio: " << p.fragment.line_i << " final: " << p.fragment.line_f <<
+         "] :: línea " << p.line << " :: Anterior: " << p.previous_word << " :: Posterior: " << p.later_word << "\n";
+        return os;
+    }
+
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,19 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
+#include <cctype>
 
 #include "../include/word.h"
 #include "../include/myfiles.h"
 
-int buscarPalabra(std::string ruta, std::string palabra, int linea_inicial, int linea_final);
+void buscarPalabra(std::string ruta, std::string palabra, int linea_inicial, int linea_final);
+std::string getRelativeWord(std::string line, int pos, bool previous);
+std::string getLineByPosition(std::string path, int position);
 
-std::vector<Word> word_found;
+std::vector<Word> words_found;
 std::vector<std::thread> vhilos;
 std::vector<std::pair<int, int>> resultados;
+std::vector<std::pair<std::string, std::string>> last_previous;
 
 int main(int argc, char *argv[])
 {
@@ -27,10 +30,9 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
+    std::string file_path = argv[1];
     std::string word = argv[2];
     int num_threads = std::stoi(argv[3]);
-
-    
 
     //int total_lines = countLines(file);
     //std::cerr << lines;
@@ -40,35 +42,67 @@ int main(int argc, char *argv[])
 
     // }
 
-    buscarPalabra(argv[1], "david", 1, 10);
+    buscarPalabra(file_path, word, 1, 12);
+    std::sort(words_found.begin(), words_found.end());
 
-    for (auto resultado : resultados) {
-        std::cout << "Palabra encontrada en la línea " << resultado.first << ", posición " << resultado.second << std::endl;
-    }
+
+    for (auto word : words_found) {
+         std::cout << word;
+     }
+
+    // for (auto resultado : resultados) {
+    //     std::cout << "Palabra encontrada en la línea " << resultado.first << ", posición " << resultado.second << std::endl;
+    // }
+
+    // for (const auto& pair : last_previous) {
+    // std::cout << "Primera: " << pair.first << " Segunda: " << pair.second << std::endl;
+    // }
+
+    // for (const auto& resultado : resultados) {
+    // std::cout << "Linea " << std::get<0>(resultado) << ", posicion " << std::get<1>(resultado) 
+    //           << ": palabra anterior '" << std::get<2>(resultado) << "', palabra posterior '" << std::get<3>(resultado) << "'" << std::endl;
+    // }
 
 
 }
 
 // Función para buscar una palabra en un archivo.
-int buscarPalabra(std::string ruta, std::string palabra, int linea_inicial, int linea_final) {
+void buscarPalabra(std::string ruta, std::string palabra, int linea_inicial, int linea_final) {
 
+    //Convertir palabra a minuscula
+    std::transform(palabra.begin(), palabra.end(), palabra.begin(), [](unsigned char c){ return std::tolower(c); });
+    
+    //Comprobamos si el fichero se abre
     std::fstream archivo = openFileRead(ruta);
     if (!archivo)
-        return EXIT_FAILURE;
-        
+        exit(1);
+
+    
     std::string linea;
     int num_linea = 1;
 
     while (std::getline(archivo, linea)) {
         if (num_linea >= linea_inicial && num_linea <= linea_final) {
+
             // Convertir la línea a minúsculas
-            std::transform(linea.begin(), linea.end(), linea.begin(), [](unsigned char c){ return std::tolower(c); });
-            
+            std::string line_lower = linea;
+            std::transform(line_lower.begin(), line_lower.end(), line_lower.begin(), [](unsigned char c){ return std::tolower(c); });
+
             // Buscar la palabra en la línea
-            size_t pos = 0;
-            while ((pos = linea.find(palabra, pos)) != std::string::npos) {
-                resultados.push_back(std::make_pair(num_linea, pos));
+            int pos = 0;
+            while ((pos = line_lower.find(palabra, pos)) != std::string::npos) {
+                resultados.push_back(std::make_pair(num_linea, pos + 1));
+                
+                std::string previous = getRelativeWord(linea, pos, false);
+                std::string last = getRelativeWord(linea, pos, true);
+
+                //std::cout << "dedf" << num_linea + 1;
+                //if (last == "") last = getRelativeWord(getLineByPosition(ruta, num_linea + 1), 0, true);
+                
                 pos += palabra.length();
+
+                Word word(1,1,1, num_linea, previous, last, pos);
+                words_found.emplace_back(word);
             }
             
         }
@@ -77,11 +111,83 @@ int buscarPalabra(std::string ruta, std::string palabra, int linea_inicial, int 
         
     }
     
-    // Cerrar el archivo y devolver los resultados
+    // Cerrar el archivo
     archivo.close();
-    return 1;
 
 }
+
+std::string getRelativeWord(std::string line, int pos, bool previous)
+{
+    bool end = false;
+    bool first_white = false;
+    bool last_white = false;
+
+    int long_word = 0;
+
+    if (pos < 0 || pos > line.size() || line.size() == 0)
+    {
+        std::cout << "Error en getRelativeWord, pos se sale de los limites o la línea está vacia\n";
+        exit(1);
+    }
+
+
+    while (!end)
+    {
+        char charac = line[pos];
+        if (charac == ' ') first_white = true;
+
+        //Encuentra el inicio de la siguiente palabra
+        if (first_white)
+            if (charac != ' ' && std::isalpha(charac)){
+                last_white = true;
+                long_word++;
+            } 
+
+        if (last_white)
+        {
+            if (charac == ' ') end = true;
+        }
+
+        if (!previous && (pos-1) > 0)
+            pos--;
+        else if (previous && (pos+1) < line.size())
+            pos++;
+        else
+            end = true;
+        
+    }
+
+    if (!previous)
+            return line.substr(pos + 2, long_word);
+        else
+            return line.substr(pos - long_word - 1, long_word);
+    
+}
+
+std::string getLineByPosition(std::string path, int position)
+{
+    //Comprobamos si el fichero se abre
+    std::fstream inputFile = openFileRead(path);
+    if (!inputFile)
+        exit(1);
+
+    std::string line;
+    int currentPos = 1;
+    while (std::getline(inputFile, line))
+    {
+        if (currentPos == position) // si se encuentra la posición buscada
+        {
+            return line; // devuelve la línea actual
+        }
+
+        currentPos++;
+    }
+    std::cout << "Line does not exist";
+    return ""; // si no se encuentra la posición, devuelve una cadena vacía
+}
+
+
+
     
    
 
